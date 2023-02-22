907ec6d03916226bb7eb6e7e87c17f6e9ced706d
João Victor <jotav.lim4@gmail.com>
2023-02-22 00:02:15 -0300

    Efetuada correção

--- exercicios_resolvidos/cap4_linguagem_c/exercicio_15.c
+++ exercicios_resolvidos/cap4_linguagem_c/exercicio_15.c
@@ -10,15 +10,19 @@ int main(){
         printf("Não é equação de segundo grau.\n");
     else{
         delta = pow(b, 2) - 4 * a * c;
+
         x1 = (- b + sqrt(delta))/(2 * a);
         x2 = (- b - sqrt(delta))/(2 * a);
         
         if(delta == 0)
             printf("x1 = x2 = %.2lf(Raiz única)\n", x1);
+
         else if(delta > 0)
             printf("x1 = %.2lf e x2 = %.2lf\n", x1, x2);
+
         else
             printf("Não existe raiz real.\n");
+            
     }
     return 0;
 }
